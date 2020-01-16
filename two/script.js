cce986c825815149ac8922573398f29aaa7dd943
Natalia <zjecutiez@gmail.com>
2020-01-16 14:59:27 +0300

    добавление максимальной температуры

--- two/script.js
+++ two/script.js
@@ -1,11 +1,12 @@
-  var term = {
-    Canada: 28,
-    Belarus: 16,
-    Russia: 18,
-    Albania: 24,
-    Germany: 21,
-    France: 17
-  }
+var term = {
+  Canada: 28,
+  Belarus: 16,
+  Russia: 18,
+  Albania: 30,
+  Germany: 21,
+  France: 17
+}
+function Country() {
   var sum = 0;
   var k = 0;
   for (var key in term) {
@@ -14,3 +15,20 @@
   }
   var average = sum / k;
   alert("Среднее значение = " + average);
+}
+Country();
+
+//Максимальная температура
+function MaxTerm() {
+  for (var key in term) {
+    var max = term[key];
+  }
+  for (var key in term) {
+    if  (max < term[key]) {
+      max = term[key];
+      count = key;
+    }
+  }
+  alert("Максимальная температура = " + max + " градусов в стране " + count);
+}
+MaxTerm();
